fb84be05185369676a7ad90482aa915d3c9a21ef
nabeelsherazi <nabeelsherazi97@gmail.com>
2018-01-28 01:12:06 -0500

    wrote server handlers for n case

--- server/server.js
+++ server/server.js
@@ -14,22 +14,32 @@ app.use('/', express.static(path.resolve(__dirname + '/../')));
 
 var initalClients = io.sockets.clients();
 var activeClients = {};
-for (var i=0; i<clients.length; i++) {
+for (var i=0; i<initalClients.length; i++) {
     activeClients[initialClients[i]] = false;
 }
 
+var counter = 0;
+var timed_message = "";
+
 io.on('connection', function(socket){
-  console.log('user with id '+ socket.id + 'connected');
+  console.log('user with id '+ socket.id + ' connected');
   activeClients[socket.id] = false;
   socket.on('disconnect', function(socket){
-      console.log('user with id '+ socket.id + 'connected');
+      console.log('user with id '+ socket.id + ' connected');
       delete activeClients.socket.id;
       });
   socket.on('chat message', function(msg){
     console.log('message from user with id '+ socket.id + ' : ' + msg);
     selectedClient = randomClient(activeClients);
     activeClients[selectedClient] = true;
-    socket.broadcast.to(selectedClient).emit('chat message', msg);
+    socket.broadcast.to(selectedClient).emit('chat message', msg, 1);
+    timed_message = msg;
+});
+
+  socket.on('heard', function(){
+    console.log('heard by user with id '+ socket.id);
+    socket.broadcast.to(socket.id).emit('chat message', msg, counter);
+    counter += 1;
 
     // for (var id in activeClients) { // Broadcast message to all people who haven't received it yet.
     //     if (activeClients[id] == false) {
