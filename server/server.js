1c549ddcb2a09dcf104cf6ee9c840f84407e7003
nabeelsherazi <nabeelsherazi97@gmail.com>
2018-01-27 23:55:42 -0500

    implement socket id dynamic recording

--- server/server.js
+++ server/server.js
@@ -4,40 +4,49 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
+const port = 3000
+
 app.get('/', function(req, res){
   res.sendFile(path.resolve(__dirname + '/../index.html'));
 });
 
 app.use('/', express.static(path.resolve(__dirname + '/../')));
 
-var clients = io.sockets.clients();
-var clients_dict = {}
-for (var i=0; i<=clients.length;i++) {
-    clients_dict[clients[i]] = false;
+var initalClients = io.sockets.clients();
+var activeClients = {};
+for (var i=0; i<clients.length; i++) {
+    activeClients[initialClients[i]] = false;
 }
 
-
 io.on('connection', function(socket){
-  console.log('a user connected');
-  clients_dict[socket.id] = false;
-  socket.on('disconnect', function(){
-  console.log('user disconnected');
-  });
+  console.log('user with id '+ socket.id + 'connected');
+  activeClients[socket.id] = false;
+  socket.on('disconnect', function(socket){
+      console.log('user with id '+ socket.id + 'connected');
+      delete activeClients.socket.id;
+      });
   socket.on('chat message', function(msg){
-    // var clients = io.sockets.clients();
-    // for (var i=0; i<=clients.length; i++) {
-    //     if (clients[i] in clients_dict) {
-    //         continue;
+    console.log('message from user with id '+ socket.id + ' : ' + msg);
+    selectedClient = randomClient(activeClients);
+    activeClients[selectedClient] = true;
+    socket.broadcast.to(selectedClient).emit('chat message', msg);
+
+    // for (var id in activeClients) { // Broadcast message to all people who haven't received it yet.
+    //     if (activeClients[id] == false) {
+    //         io.to(id).emit('chat message', msg);
     //     }
     //     else {
-    //         clients_dict[clients[i]] = false;
+    //         continue;
     //     }
-    console.log(clients)
-    console.log('message: ' + msg);
-    socket.broadcast.to(socket.id).emit('chat message', msg);
+    // };
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
+
+var randomClient = function (obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
